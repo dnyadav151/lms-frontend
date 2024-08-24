cd0de362f78801a6840b386a427a2fbd2736a96a
dhruv151 <dnyadav209@gmail.com>
2024-08-25 00:34:34 +0530

    Set up of axios instance done

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 export default [
   {
@@ -20,6 +21,7 @@ export default [
     settings: { react: { version: '18.3' } },
     plugins: {
       react,
+      'simple-import-sort': simpleImportSort,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
@@ -28,6 +30,7 @@ export default [
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
+      'simple-import-sort/imports': 'error',
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
