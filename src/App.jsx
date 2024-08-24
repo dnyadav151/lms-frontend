b679b64b132cbd81e0b9701ee36d1ed099a9d05f
dhruv151 <dnyadav209@gmail.com>
2024-08-24 18:57:08 +0530

    react hot toast set up

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+          {
+            /* <Route path = "/" element={<Home />} </Route> */
+          }
+      </Routes>
+      LMS
+    </>
+  )
+}
+
+export default App
